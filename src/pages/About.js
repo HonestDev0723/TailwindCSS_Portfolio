7e3c3ca4774ff4a8e31884ac447fed79e1b6b741
HonestDev0723 <khranovskyioleksii@gmail.com>
2024-04-01 01:24:28 +0900

    change about page

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import WhoIM from "../components/WhoIM";
+import Github from "../components/Github";
+import Techstack from "../components/Techstack";
+import Toolstack from "../components/Toolstack";
+
+function About() {
+  return (
+    <div>
+      <WhoIM />
+      <h1 className="m-auto flex items-center w-full justify-center gap-2 text-gray-200 font-medium text-4xl my-3">
+        Professional <p className="text-[#ad50eb]">Skillset </p>
+      </h1>
+      <Techstack />
+      <h1 className="m-auto flex items-center w-full justify-center gap-2 text-gray-200 font-medium text-4xl my-3">
+        <p className="text-[#ad50eb]">Tools </p> I Use
+      </h1>
+      <Toolstack />
+      <h1 className="m-auto flex items-center w-full justify-center gap-2 text-gray-200 font-medium text-4xl my-3">
+        Days I<p className="text-[#ad50eb]">Code </p>
+      </h1>
+      <Github />
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Interduce from "../components/Interduce";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Interduce />
+    </div>
+  );
+}
+
+export default Home;
